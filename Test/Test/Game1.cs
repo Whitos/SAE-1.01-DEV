7aee6938b109f502fb5475df725af065817601f0
milhane rabehi <milhane.rabehi@etu.univ-smb.fr>
2023-01-04 15:23:43 +0100

    personnage amelioration1

--- Test/Test/Game1.cs
+++ Test/Test/Game1.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
+using System;
 
 namespace Test
 {
@@ -15,8 +18,23 @@ namespace Test
 
         private TiledMap _TiledManoirEXT;
 
+        //personnage
         private Vector2 _positionPerso;
         private AnimatedSprite _perso;
+        private TiledMapTileLayer mapLayer;
+        private int vitesse;
+        String animation = "idle";
+
+        private bool IsCollision(ushort x, ushort y)
+        {
+            // définition de tile qui peut être null (?)
+            TiledMapTile? tile;
+            if (mapLayer.TryGetTile(x, y, out tile) == false)
+                return false;
+            if (!tile.Value.IsBlank)
+                return true;
+            return false;
+        }
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +46,11 @@ namespace Test
         {
             // TODO: Add your initialization logic here
             Window.Title = "Nom de fenetre";
+
+            //personnage
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _positionPerso = new Vector2(20, 340);
+            vitesse = 50;
             base.Initialize();
         }
 
@@ -37,6 +60,9 @@ namespace Test
 
             // TODO: use this.Content to load your game content here
             backgroundSprite = Content.Load<Texture2D>("manoir");
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("personnage.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spriteSheet);
+            mapLayer = _TiledManoirEXT.GetLayer<TiledMapTileLayer>("obstacles");
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,11 +70,62 @@ namespace Test
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float walkSpeed = deltaSeconds * vitesse; // Vitesse de déplacement du sprite
+            KeyboardState keyboardState = Keyboard.GetState();
+
+
+            animation = "idle";
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _TiledManoirEXT.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _TiledManoirEXT.TileHeight - 1);
+                animation = "walkNorth";
+                if (!IsCollision(tx, ty))
+                    _positionPerso.Y -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _TiledManoirEXT.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _TiledManoirEXT.TileHeight + 1);
+                animation = "walkSouth";
+                if (!IsCollision(tx, ty))
+                    _positionPerso.Y += walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _TiledManoirEXT.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _TiledManoirEXT.TileHeight + 1);
+                animation = "walkWest";
+                if (!IsCollision(tx, ty))
+                    _positionPerso.X -= walkSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _TiledManoirEXT.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _TiledManoirEXT.TileHeight + 1);
+                animation = "walkEast";
+                if (!IsCollision(tx, ty))
+                    _positionPerso.X += walkSpeed;
+            }
+
             // TODO: Add your update logic here
+            _TiledManoirEXT.Update(gameTime);
+            _perso.Play(animation); // une des animations définies dans « persoAnimation.sf »
+            _perso.Update(deltaSeconds); // time écoulé
+
+            // TODO: Add your update logic here
+            _TiledManoirEXT.Update(gameTime);// une des animations définies dans « persoAnimation.sf »
+            _perso.Update(deltaSeconds); // time écoulé
+            base.Update(gameTime);
 
             base.Update(gameTime);
         }
-        
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Red);
@@ -56,6 +133,11 @@ namespace Test
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
+
+            //personnage
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _positionPerso);
             _spriteBatch.End();
 
             base.Draw(gameTime);
