a1baa87342bf03c5ccd44e235e38f43d937a32c3
Pavel Tsanev <Pavel.Tsanev@etu.univ-savoie.fr>
2023-01-05 14:09:10 +0100

    classe perso

--- Test/Test/Game1.cs
+++ Test/Test/Game1.cs
@@ -22,18 +22,9 @@ namespace Test
         public SpriteBatch SpriteBatch { get; private set; }
         private ScreenManager _screenManager;
 
-        //personnage
-        private Vector2 _positionPerso;
-        private AnimatedSprite _perso;
-        public float _vitesse;
-        private float _vitessePerso;
-        private int _sensPersoX;
-        private int _sensPersoY;
-
         //MapExt
         //private TiledMap _tiledMap;
         //private TiledMapRenderer _tiledMapRenderer;
-
 
         //clavier 
         private KeyboardState _keyboardState;
@@ -54,12 +45,6 @@ namespace Test
 
             //personnage
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
-            _positionPerso = new Vector2(20, 340);
-            _vitesse = 70;
-            _vitessePerso = 70;
-
-
-
             base.Initialize();
         }
 
@@ -69,10 +54,10 @@ namespace Test
 
         }
 
-        public void LoadMapExt()
-        {
-            LoadScreen(new MapExt(this));
-        }
+        //public void LoadMapExt()
+        //{
+        //    LoadScreen(new MapExt(this));
+        //}
 
         protected override void LoadContent()
         {
@@ -84,13 +69,10 @@ namespace Test
             backgroundSprite = Content.Load<Texture2D>("manoir");
 
             //MAP EXT
-            LoadMapExt();
+            //LoadMapExt();
             //_tiledMap = Content.Load<TiledMap>("mapGenerale");
             //_tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
 
-            //personnage
-            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("Personnage.sf", new JsonContentLoader());
-            _perso = new AnimatedSprite(spriteSheet);
             
         }
 
@@ -100,66 +82,8 @@ namespace Test
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            //_tiledMapRenderer.Update(gameTime);
-
-            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            float walkSpeed = deltaSeconds * _vitessePerso; // Vitesse de déplacement du sprite
-
-            _keyboardState = Keyboard.GetState();
+            // TODO: Add your update logic here      
             _screenManager.Update(gameTime);
-
-            _sensPersoX = 0;
-            _sensPersoY = 0;
-
-            if (_keyboardState.IsKeyDown(Keys.Right) && !(_keyboardState.IsKeyDown(Keys.Left)))
-            {
-                _sensPersoX = 1;
-            }
-
-            //si fleche gauche
-            else if (_keyboardState.IsKeyDown(Keys.Left) && !(_keyboardState.IsKeyDown(Keys.Right)))
-            {
-                _sensPersoX = -1;
-            }
-
-            if (_keyboardState.IsKeyDown(Keys.Up) && !(_keyboardState.IsKeyDown(Keys.Down)))
-            {
-                _sensPersoY = -1;
-            }
-
-            else if (_keyboardState.IsKeyDown(Keys.Down) && !(_keyboardState.IsKeyDown(Keys.Up)))
-            {
-                _sensPersoY = 1;
-            }
-
-            _vitessePerso = _vitesse;
-
-            if (_sensPersoX != 0 && _sensPersoY != 0)
-                _vitessePerso = _vitesse / (float)Math.Sqrt(2);
-            else
-                _vitessePerso = _vitesse;
-
-            if (_sensPersoX == 0 && _sensPersoY == 0)
-                _perso.Play("idle");
-
-            else if (_sensPersoX == 1 && _sensPersoY == 0)
-                _perso.Play("walkEast");
-
-            else if (_sensPersoX == -1 && _sensPersoY == 0)
-                _perso.Play("walkWest");
-
-            //Si cest vers le bas = → && ↓ || ← && ↓ || 0 && ↓
-            else if (_sensPersoX == 1 && _sensPersoY == 1 || _sensPersoX == -1 && _sensPersoY == 1 || _sensPersoX == 0 && _sensPersoY == 1)
-                _perso.Play("walkSouth");
-
-            //Si cest vers le bas = → && ↑ || ← && ↑ || 0 && ↑
-            else if (_sensPersoX == 1 && _sensPersoY == -1 || _sensPersoX == -1 && _sensPersoY == -1 || _sensPersoX == 0 && _sensPersoY == -1)
-                _perso.Play("walkNorth");
-
-            _perso.Update(deltaSeconds);
-            _positionPerso.X += _sensPersoX * _vitessePerso * deltaSeconds;
-            _positionPerso.Y += _sensPersoY * _vitessePerso * deltaSeconds;
 
             base.Update(gameTime);
         }
@@ -167,18 +91,8 @@ namespace Test
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
-
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
-            
+            // TODO: Add your drawing code here           
             _screenManager.Draw(gameTime);
-           
-            _spriteBatch.Draw(_perso, _positionPerso);
-
-            _spriteBatch.End();
-
-
             base.Draw(gameTime);
         }
     }
---
+++ Test/Test/Perso.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Perso
+    {
+        //personnage
+        private Vector2 _positionPerso;
+        private AnimatedSprite _perso;
+        public float _vitesse;
+        private float _vitessePerso;
+        private int _sensPersoX;
+        private int _sensPersoY;
+
+        private KeyboardState _keyboardState;
+
+        public Vector2 Position { get => _positionPerso; set => _positionPerso = value; }
+
+        public void LoadContent(Game game1)
+        {
+            SpriteSheet mike = game1.Content.Load<SpriteSheet>("Personnage.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(mike);
+        }
+
+        public void Update(float deltaTime)
+        {
+            _keyboardState = Keyboard.GetState();
+            _sensPersoX = 0;
+            _sensPersoY = 0;
+
+            if (_keyboardState.IsKeyDown(Keys.Right) && !(_keyboardState.IsKeyDown(Keys.Left)))
+            {
+                _sensPersoX = 1;
+            }
+
+            //si fleche gauche
+            else if (_keyboardState.IsKeyDown(Keys.Left) && !(_keyboardState.IsKeyDown(Keys.Right)))
+            {
+                _sensPersoX = -1;
+            }
+
+            if (_keyboardState.IsKeyDown(Keys.Up) && !(_keyboardState.IsKeyDown(Keys.Down)))
+            {
+                _sensPersoY = -1;
+            }
+
+            else if (_keyboardState.IsKeyDown(Keys.Down) && !(_keyboardState.IsKeyDown(Keys.Up)))
+            {
+                _sensPersoY = 1;
+            }
+
+            _vitessePerso = _vitesse;
+
+            if (_sensPersoX != 0 && _sensPersoY != 0)
+                _vitessePerso = _vitesse / (float)Math.Sqrt(2);
+            else
+                _vitessePerso = _vitesse;
+
+            if (_sensPersoX == 0 && _sensPersoY == 0)
+                _perso.Play("idle");
+
+            else if (_sensPersoX == 1 && _sensPersoY == 0)
+                _perso.Play("walkEast");
+
+            else if (_sensPersoX == -1 && _sensPersoY == 0)
+                _perso.Play("walkWest");
+
+            //Si cest vers le bas = → && ↓ || ← && ↓ || 0 && ↓
+            else if (_sensPersoX == 1 && _sensPersoY == 1 || _sensPersoX == -1 && _sensPersoY == 1 || _sensPersoX == 0 && _sensPersoY == 1)
+                _perso.Play("walkSouth");
+
+            //Si cest vers le bas = → && ↑ || ← && ↑ || 0 && ↑
+            else if (_sensPersoX == 1 && _sensPersoY == -1 || _sensPersoX == -1 && _sensPersoY == -1 || _sensPersoX == 0 && _sensPersoY == -1)
+                _perso.Play("walkNorth");
+
+            _perso.Update(deltaTime);
+            _positionPerso.X += _sensPersoX * _vitessePerso * deltaTime;
+            _positionPerso.Y += _sensPersoY * _vitessePerso * deltaTime;
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _positionPerso);
+            _spriteBatch.End();
+        }
+    }
+}
